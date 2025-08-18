802dec29882f6de0e94ee03083602e0c62aa1dbf
游家纨绔 <youshicheng97@163.com>
2025-08-19 00:07:14 +0800

    代码提交，无重大版本更迭

--- Vue2/07_Vue2-Pinia/src/views/PiniaView2.vue
+++ Vue2/07_Vue2-Pinia/src/views/PiniaView2.vue
@@ -2,33 +2,23 @@
   <div class="pinia2">
     <fieldset>
       <legend>
-        {{ counter }}
-        <button @click="increment">{{ counter }}</button>
+        {{ counterStore.counter }}
+        <button @click="counterStore.increment()">{{ counterStore.counter }}</button>
       </legend>
     </fieldset>
   </div>
 </template>
 
 <script>
-// import {mapState, mapActions,} from 'pinia'
-import storeId from '../store'  // storeId就是导出的那个值
-
-const ustoreId = storeId() // 实例化容器
-console.log(ustoreId)
-const {counter} = ustoreId // 解构出来的值是不能动态绑定的 可以通过下面的方法
+import useCounterStore from '../store'
 
 export default {
-  data() {
-    return {
-      id: ''
-    }
-  },
-  methods: {},
-  computed: {},
-  mounted() {
-    this.id = counter
+  // TODO 这里使用到了 Vue3 中的组合式API的概念
+  setup() {
+    const counterStore = useCounterStore()
+    return { counterStore }
   }
-};
+}
 </script>
 
 
--- Vue2/07_Vue2-Pinia/src/views/PiniaView3.vue
+++ Vue2/07_Vue2-Pinia/src/views/PiniaView3.vue
@@ -2,20 +2,22 @@
   <div class="pinia3">
     <fieldset>
       <legend>
-        {{ mainStore.counter }}
-        <button @click="mainStore.increment">{{ mainStore.counter }}</button>
+        {{ id }}
+        <button @click="increment">{{ id }}</button>
       </legend>
     </fieldset>
   </div>
 </template>
 
 <script>
+// storeToRefs 是一个工具函数，它接受一个 store 对象作为参数，并返回一个包含 store 中所有响应式数据的对象。
+// 这个对象的每个属性都是一个 ref 对象，对应 store 中的相应数据。【简单理解：将 store 中的数据转换为响应式的 ref 对象】
 import {storeToRefs} from 'pinia'
 import storeId from '../store'  // storeId就是导出的那个值
 
 const ustoreId = storeId()      // 实例化容器
 console.log(ustoreId)
-const {counter} = storeToRefs(ustoreId) // 这样使用
+const {counter, increment} = storeToRefs(ustoreId) // 解构出需要使用的属性
 
 export default {
   data() {
@@ -23,7 +25,11 @@ export default {
       id: ''
     }
   },
-  methods: {},
+  methods: {
+    increment() {
+      increment(this.id)
+    }
+  },
   computed: {},
   mounted() {
     this.id = counter  // 但是这样出来的值会变成一个对象 值的key 为value 使用的时候需要注意
