b718311cc9b65728b835f31c16e92a07907a3a57
游家纨绔 <youshicheng97@163.com>
2025-08-19 23:33:59 +0800

    代码提交，无重大版本更迭

--- Vue2/07_Vue2-Pinia/src/router/index.js
+++ Vue2/07_Vue2-Pinia/src/router/index.js
@@ -41,14 +41,6 @@ const routes = [
     // this generates a separate chunk (new.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "new" */ '../views/PiniaView3.vue')
-  },
-  {
-    path: '/pinia4',
-    name: 'pinia4',
-    // route level code-splitting
-    // this generates a separate chunk (new.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "new" */ '../views/PiniaView4.vue')
   }
 ]
 
--- Vue2/07_Vue2-Pinia/src/views/PiniaView2.vue
+++ Vue2/07_Vue2-Pinia/src/views/PiniaView2.vue
@@ -2,25 +2,25 @@
   <div class="pinia2">
     <fieldset>
       <legend>
-        {{ counterStore.counter }}
-        <button @click="counterStore.increment()">{{ counterStore.counter }}</button>
+        {{ mainStore.counter }}
+        <button @click="mainStore.increment">{{ mainStore.counter }}</button>
       </legend>
     </fieldset>
   </div>
 </template>
 
 <script>
-import useCounterStore from '../store'
+// 导入 pinia 实例
+import useMainStore from '../store'
 
 export default {
-  // TODO 这里使用到了 Vue3 中的组合式API的概念
-  setup() {
-    const counterStore = useCounterStore()
-    return { counterStore }
+  data() {
+    return {
+      mainStore: useMainStore()
+    }
   }
-}
+};
 </script>
-
 
 <style scoped>
 .pinia2 {
